ae5f8f637d4eb80a865b0c5282b8862b00a4ae54
Utkarsh Singh <30589139+Utkarsh1999@users.noreply.github.com>
2020-03-20 07:27:38 +0530

    Remove a minor typo bug

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ io.on('connection',(socket)=>{
     socket.on('disconnect',() => {
         const user = removeUser(socket.id);
         if(user){
-            io.to(user.room).emit('message',{user:'admin', text: `$user.name has left.`})
+            io.to(user.room).emit('message',{user:'admin', text: `${user.name} has left.`})
             console.log("users in room : ",getUsersInRoom(user.room));
         }
         console.log("user had left!!!");
